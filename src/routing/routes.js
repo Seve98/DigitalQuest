b9bf58ee1e4feaa7196601b3bf1821c370a4c8b5
Seve98 <seve.98@hotmail.it>
2025-10-16 17:35:10 +0200

    Fix: HomePage

--- src/routing/routes.js
+++ src/routing/routes.js
@@ -1,40 +1,30 @@
+import { createBrowserRouter } from "react-router";
 
+import Layout from "../layout/Layout";
+import HomePage from "../pages/homepage/HomePage.jsx";
+import GenrePage from "../pages/genrepage/GenrePage";
+import GameDetails from "../pages/details/GameDetails";
+import SearchPage from "../pages/search/SearchPage";
+import Register from "../pages/register/register";
+import Login from "../pages/login/Login";
+import Account from "../pages/account/Account";
+import Profile from "../pages/profile/Profile";
 
-import { createBrowserRouter } from "react-router"
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    Component: Layout,
+    children: [
+      { index: true, Component: HomePage },
+      { path: "games/:genre", Component: GenrePage },
+      { path: "games/:slug/:id", Component: GameDetails },
+      { path: "search", Component: SearchPage },
+      { path: "register", Component: Register },
+      { path: "login", Component: Login },
+      { path: "account", Component: Account },
+      { path: "profile", Component: Profile }
+    ]
+  }
+]);
 
-
-
-import Layout from "../layout/Layout.jsx"
-import GenrePage from "../pages/genrepage/GenrePage.jsx"
-import GameDetails from "../pages/details/GameDetails.jsx"
-import SearchPage from "../pages/search/SearchPage.jsx"
-import Register from "../pages/register/Register.jsx"
-import Login from "../pages/login/Login.jsx"
-import Account from "../pages/account/Account.jsx"
-import Profile from "../pages/profile/Profile.jsx"
-import HomePage from "../pages/homepage/HomePage.jsx"
-
-
-
-
-
-const routes=createBrowserRouter([
-    {
-        path:"/",
-        Component:Layout, 
-        children:[
-                   {index:true, Component: HomePage },
-                   {path:"games/:genre", Component:GenrePage},
-                   {path:"games/:slug/:id", Component:GameDetails},
-                   {path:"/search",Component:SearchPage},
-                   {path:"/register",Component:Register},
-                   {path:"/login",Component:Login},
-                   {path:"/account",Component:Account},
-                   {path:"/profile",Component:Profile}
-                  
-                 ]
-    },
-   
-    
-])
-export default routes
+export default routes;
